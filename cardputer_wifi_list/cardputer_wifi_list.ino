6f40c5ebf5c972482fd336e097dccccae46894f9
T14g <ottiagos@gmail.com>
2024-01-05 23:36:57 -0300

    friday coding

--- cardputer_wifi_list/cardputer_wifi_list.ino
+++ cardputer_wifi_list/cardputer_wifi_list.ino
@@ -11,17 +11,19 @@ void displayNetworks() {
   M5.Lcd.setTextSize(1);
 
   int numNetworks = WiFi.scanNetworks();
-  if (numNetworks == 0) {
-    M5.Lcd.println("No WiFi networks found.");
-  } else {
-    M5.Lcd.println("Available WiFi Networks:");
+  // if (numNetworks == 0) {
+  //   M5.Lcd.println("No WiFi networks found.");
+  // } else {
+  //   M5.Lcd.println("Available WiFi Networks:");
 
-    for (int i = startIdx; i < min(startIdx + MAX_NETWORKS, numNetworks); ++i) {
-      int yPos = (i - startIdx) * 15; // Adjust the vertical spacing as needed
-      M5.Lcd.setCursor(10, yPos);
-      M5.Lcd.printf("%d: %s (RSSI: %d dBm)", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i));
-    }
-  }
+  //   for (int i = startIdx; i < min(startIdx + MAX_NETWORKS, numNetworks); ++i) {
+  //     int yPos = (i - startIdx) * 15; // Adjust the vertical spacing as needed
+  //     M5.Lcd.setCursor(10, yPos);
+  //     M5.Lcd.printf("%d: %s (RSSI: %d dBm)", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i));
+  //   }
+  // }
+
+  Serial.println(numNetworks);
 }
 
 void setup() {
@@ -34,7 +36,7 @@ void setup() {
 
 void loop() {
   M5.update();
-
+  int numNetworks = WiFi.scanNetworks();
   // if (M5.BtnA.wasPressed()) {
   //   selectedNetwork = max(0, selectedNetwork - 1);
   //   if (selectedNetwork < startIdx) {
@@ -43,14 +45,15 @@ void loop() {
   //   displayNetworks();
   // }
 
-  if (M5.BtnA.wasPressed()) {
-    int numNetworks = WiFi.scanNetworks();
-    if (numNetworks > 0) {
-      selectedNetwork = min(selectedNetwork + 1, numNetworks - 1);
-      if (selectedNetwork >= startIdx + MAX_NETWORKS) {
-        startIdx = selectedNetwork - MAX_NETWORKS + 1;
-      }
-      displayNetworks();
-    }
-  }
+  // if (M5.BtnA.wasPressed()) {
+  //   int numNetworks = WiFi.scanNetworks();
+  //   if (numNetworks > 0) {
+  //     selectedNetwork = min(selectedNetwork + 1, numNetworks - 1);
+  //     if (selectedNetwork >= startIdx + MAX_NETWORKS) {
+  //       startIdx = selectedNetwork - MAX_NETWORKS + 1;
+  //     }
+  //     displayNetworks();
+  //   }
+  // }
+  Serial.println(numNetworks);
 }
