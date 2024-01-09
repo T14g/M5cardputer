45fe39545fd267227c5c076b0d764bf8772b1b8c
T14g <ottiagos@gmail.com>
2024-01-09 00:50:24 -0300

    sending diff IR commands when press key 1 or 2

--- ir_sender/ir_sender.ino
+++ ir_sender/ir_sender.ino
@@ -1,14 +1,21 @@
 #define DISABLE_CODE_FOR_RECEIVER  // Disables restarting receiver after each
                                    // send. Saves 450 bytes program memory and
                                    // 269 bytes RAM if receiving functions are
-                                   // not used.
+                                   // not use
+
+
+// #define ENABLE_NEC2_REPEATS
+
 #define SEND_PWM_BY_TIMER
 #define IR_TX_PIN 44
 
 #include "M5Cardputer.h"
 #include <IRremote.hpp>  // include the library
 
+bool enviar = true;
+
 void setup() {
+  Serial.begin(9600);
     auto cfg = M5.config();
     M5Cardputer.begin(cfg, true);
     M5Cardputer.Display.setRotation(1);
@@ -22,11 +29,27 @@ void setup() {
 }
 
 void loop() {
-    M5Cardputer.Display.clear();
-    M5Cardputer.Display.drawString("IR NEC SEND",
-                                   M5Cardputer.Display.width() / 2,
-                                   M5Cardputer.Display.height() / 2 - 40);
 
-    IrSender.sendNEC(0xEF00 , 0x5 , 40700);
-    delay(500);
+
+    M5Cardputer.update();
+    if (M5Cardputer.Keyboard.isChange()) {
+        if (M5Cardputer.Keyboard.isKeyPressed('/')) {
+            M5Cardputer.Display.clear();
+            M5Cardputer.Display.drawString("Right Pressed",
+                                           M5Cardputer.Display.width() / 2,
+                                           M5Cardputer.Display.height() / 2);
+
+                                            IrSender.sendNEC(0xEF00 , 0x5 , 40700);
+   
+      // enviar = false;
+         delay(500);
+                                      
+
+
+        }else {
+           M5Cardputer.Display.clear();
+        }
+    }
+
+    Serial.println("hello world");
 }
